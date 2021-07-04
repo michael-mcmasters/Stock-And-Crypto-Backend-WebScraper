395c6369b42d2b05f367ddd57afd68543fbef826
Michael McMasters <jmcmasters411@gmail.com>
2021-07-04 01:39:06 -0400

    Clean up

--- src/main/java/com/mcmasters/webscraper/controllers/CryptoController.java
+++ src/main/java/com/mcmasters/webscraper/controllers/CryptoController.java
@@ -5,11 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
-@RestController
+@RestController()
 public class CryptoController {
 
     @Autowired
