2d3c559152697cdb0818fed82a79b3c666a86a7c
Michael McMasters <jmcmasters411@gmail.com>
2021-07-16 00:39:58 -0400

    Add price history to Stock object

--- src/main/java/com/mcmasters/webscraper/Entities/Stock.java
+++ src/main/java/com/mcmasters/webscraper/Entities/Stock.java
@@ -3,12 +3,28 @@ package com.mcmasters.webscraper.Entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 public class Stock {
 
     private String tickerName;
     private String price;
-    private String priceDifference;
-    private String percentageDifference;
+    Map<String, String> historicPrices;
+
+//    private String dayPrice;
+//    private String dayPercentage;
+//
+//    private String weekPrice;
+//    private String weekPercentage;
+//
+//    private String monthPrice;
+//    private String monthPercentage;
+//
+//    private String YTDPrice;
+//    private String YTDPercentage;
+//
+//    private String yearPrice;
+//    private String yearPercentage;
 }
--- src/main/java/com/mcmasters/webscraper/services/Scraper.java
+++ src/main/java/com/mcmasters/webscraper/services/Scraper.java
@@ -9,6 +9,9 @@ import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class Scraper {
@@ -21,15 +24,45 @@ public class Scraper {
     // Calculate percentage this way
 
     public Stock scrapeCyptoPrice(String coin, String time) throws IOException {
-        String uri = "https://robinhood.com/crypto/" + coin;
-        Document document = Jsoup.connect(uri).get();
+//        String uri = "https://robinhood.com/crypto/" + coin;
+//        Document document = Jsoup.connect(uri).get();
+//
+//        String price = getCurrentPrice(document);
+//        Tuple<String, String> difference = get1DPercentageDifference(document);
+//        String priceDifference = difference.x;
+//        String percentageDifference = difference.y;
+//
+//        return new Stock(coin, price, priceDifference, percentageDifference);
 
-        String price = getCurrentPrice(document);
-        Tuple<String, String> difference = get1DPercentageDifference(document);
-        String priceDifference = difference.x;
-        String percentageDifference = difference.y;
 
-        return new Stock(coin, price, priceDifference, percentageDifference);
+
+//        String currentPriceURI = "https://robinhood.com/crypto/" + coin;
+//        String historicPriceURI = getHistoricPriceURI(coin, false);
+//        Document currentPriceDoc = Jsoup.connect(currentPriceURI).get();
+//        Document historicPriceDoc = Jsoup.connect(historicPriceURI).get();
+
+        Document currentPriceDoc = Jsoup.connect("https://robinhood.com/crypto/" + coin).get();
+        Document historicPriceDoc = Jsoup.connect(getHistoricPriceURI(coin, false)).get();
+
+        String price = getCurrentPrice(currentPriceDoc);
+        Map<String, String> historicPrices = getHistoricPrices(currentPriceDoc, historicPriceDoc);
+        return new Stock(coin, price, historicPrices);
+
+
+
+//        String uri = "https://robinhood.com/crypto/" + coin;
+//        Document document = Jsoup.connect(uri).get();
+//
+//        String currentPrice = getCurrentPrice(document);
+//        List<Tuple<String, String>> list = getHistoryAndPercentages(document);
+//
+//        Stock s = new Stock();
+//        s.setDayPrice(list.get(0).x);
+//        s.setDayPrice(list.get(0).y);
+//        s.setMonthPrice(list.get(0).x);
+//        s.setMonthPrice(list.get(0).y);
+//        // etc ...
+
 
 
 //        String price = scrape("https://robinhood.com/crypto/" + coin);
@@ -48,6 +81,32 @@ public class Scraper {
             priceStr = priceStr.substring(1);
         }
         return priceStr;
+    }
+
+    private String getHistoricPriceURI(String ticker, boolean isStock) {
+        if (isStock) {
+            return "https://www.barchart.com/stocks/quotes/" + ticker + "/performance";
+        } else {
+            return "https://www.barchart.com/crypto/quotes/%5E" + ticker + "USD/performance";
+        }
+    }
+
+    private Map<String, String> getHistoricPrices(Document currentPriceDoc, Document historicePriceDoc) {
+        Map<String, String> map = new HashMap<>();
+
+        String percentageStr = currentPriceDoc.selectFirst("._27rSsse3BjeLj7Y1bhIE_9").text();
+        String[] arr = percentageStr.split(" ");
+        map.put(arr[0], arr[1]);
+
+        Elements elements = historicePriceDoc.select(".odd");
+        for (Element e : elements) {
+            arr = e.text().split(" ");
+            System.out.println(arr[0]);
+            System.out.println(arr[10]);
+            System.out.println("\b");
+            map.put(arr[0], arr[10]);
+        }
+        return map;
     }
 
     private Tuple<String, String> get1DPercentageDifference(Document document) {
