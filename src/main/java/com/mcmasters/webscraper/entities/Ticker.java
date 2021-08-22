dffb5ebe9990cca3556a4a1c9170c4158eaad53b
Michael McMasters <jmcmasters411@gmail.com>
2021-08-22 19:04:17 -0400

    Clean up how hashmap assigns historic prices

--- src/main/java/com/mcmasters/webscraper/entities/Ticker.java
+++ src/main/java/com/mcmasters/webscraper/entities/Ticker.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-// A Ticker can be a Stock or Crypto.
+// A Ticker can be a Stock or a Crypto.
 
 @Slf4j
 @Data
@@ -28,15 +28,10 @@ public class Ticker {
         if (priceChangesList.size() < 4)
             log.warn("Expected priceChanges list to have a size of 4 but it instead has a size of {}", priceChangesList.size());
 
-        if (priceChangesList.size() > 0)
-            priceChanges.put("day", priceChangesList.get(0));
-        if (priceChangesList.size() > 1)
-            priceChanges.put("week", priceChangesList.get(1));
-        if (priceChangesList.size() > 2)
-            priceChanges.put("month", priceChangesList.get(2));
-        if (priceChangesList.size() > 3)
-            priceChanges.put("ytd", priceChangesList.get(3));
-        if (priceChangesList.size() > 4)
-            priceChanges.put("year", priceChangesList.get(4));
+        priceChanges.put("day", priceChangesList.size() > 0 ? priceChangesList.get(0) : null);
+        priceChanges.put("week", priceChangesList.size() > 1 ? priceChangesList.get(1) : null);
+        priceChanges.put("month", priceChangesList.size() > 2 ? priceChangesList.get(2) : null);
+        priceChanges.put("ytd", priceChangesList.size() > 3 ? priceChangesList.get(3) : null);
+        priceChanges.put("year", priceChangesList.size() > 4 ? priceChangesList.get(4) : null);
     }
 }
