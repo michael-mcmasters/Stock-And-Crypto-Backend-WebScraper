f91f2ca937c9a1d6f0f4c9a5203fe32a03986745
Michael McMasters <jmcmasters411@gmail.com>
2021-08-22 12:45:38 -0400

    Refactor method name formatting

--- src/main/java/com/mcmasters/webscraper/services/WebScraper.java
+++ src/main/java/com/mcmasters/webscraper/services/WebScraper.java
@@ -2,12 +2,9 @@ package com.mcmasters.webscraper.services;
 
 import com.mcmasters.webscraper.entities.HistoricPrice;
 import com.mcmasters.webscraper.entities.Stock;
-import com.mcmasters.webscraper.utils.StringConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -46,29 +43,22 @@ public class WebScraper {
         }
     }
 
-
-
     // Scrapes the given websites for the given stock/crypto ticker and returns data as a Stock object.
     private Stock scrape(String ticker, String type, String robinhoodURI, String barchartURI) throws IOException {
         log.info("Scraping for robinhoodURI = {} and barchartURI = {}", robinhoodURI, barchartURI);
         Document robinhoodDoc = Jsoup.connect(robinhoodURI).get();
         Document barchartDoc = Jsoup.connect(barchartURI).get();
 
-        double price = getCurrentPrice(robinhoodDoc);
+        double price = robinhoodScraperService.scrapeCurrentPrice(robinhoodDoc);
+        List<HistoricPrice> historicPrices = scrapeHistoricPrices(robinhoodDoc, barchartDoc);
         log.info("Price = {}", price);
-
-        List<HistoricPrice> historicPrices = getHistoricPrices(robinhoodDoc, barchartDoc);
         log.info("historicPrices = {}", historicPrices);
 
         return new Stock(ticker, type, price, historicPrices);
     }
 
-    private double getCurrentPrice(Document robinhoodDoc) {
-        return robinhoodScraperService.scrapeCurrentPrice(robinhoodDoc);
-    }
-
     // Returns list of historic prices. 1D price is scraped from Robinhood, 5D/week/month etc is scraped from barchart.
-    private List<HistoricPrice> getHistoricPrices(Document robinhoodDoc, Document barchartDoc) {
+    private List<HistoricPrice> scrapeHistoricPrices(Document robinhoodDoc, Document barchartDoc) {
         List<HistoricPrice> historicPrices = new ArrayList<>();
 
         // ToDo: Maybe change HistoricPrice name to PriceDifference?
